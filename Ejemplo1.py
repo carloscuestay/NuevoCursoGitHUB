9a75c629c3aa87f5d1153432bf285bdc4a63799f
CACY <carloscuestay@gmail.com>
2023-03-08 21:34:26 -0500

    LISTO LOGIN

---
+++ Ejemplo1.py
@@ -0,0 +1,8 @@
+import math
+
+print("\nEste es el primer ejemplo, captura un dato y lo suma y presenta en pantalla\n")
+Result = eval(input ("Favor digite un numero\n"))
+Result =  Result + 50
+print("El resultado fue modificado adicionando valor real: ",Result)
+RaizC = math.sqrt(Result)
+print("La Raiz Cuadrada es: ", RaizC) 
---
+++ Ejemplo2.py
@@ -0,0 +1,10 @@
+print("Ejemplo de Sentencias de control\n")
+Num1= eval(input("Digita primer nuemero\n"))
+Num2 = eval(input("Digite segundo numero\n"))
+if Num1>Num2:
+    print ("El primer numero",Num1,"es mayor que el segundo")
+elif Num1<Num2:
+    print ("El primer numero", Num1, "es menor que el segundo")
+else:
+    print ("Los numeros son iguales")
+    
